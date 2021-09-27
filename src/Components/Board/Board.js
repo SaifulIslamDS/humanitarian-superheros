cfe2edfca69ef052edf376a7407baec26f3aa813
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-09-27 14:05:39 +0600

    Design update in board section

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -13,15 +13,24 @@ const Board = (props) => {
             <h2> <i className="fas fa-clipboard"></i> Fund Board</h2>
             <div className="card h-100">
             <div className="card-body">
-                <h3 className="card-name">Package added: {board.length}</h3>
-                <p>Total Donation: {total} M</p>
+                <h4 className="card-name">Package added: {board.length}</h4>
+                <h6>Total Donation: {total} M</h6>
             </div>
             <div className="card-footer">
-                <ol className="ordered-list">
+                <div className="list">
                     {
-                        board.map((item) => <li className="list-item">{item.name}, ${item.donation} M</li> )
+                        board.map((item) => <div className="list-item d-flex">
+                                <div>
+                                    <img className="img-custom" src={item.img} alt="img"/>
+                                </div>
+                                <div className="item-meta">
+                                    <h6>{item.name}</h6> 
+                                    <p>${item.donation} M</p>
+                                </div>
+                            </div> 
+                            )
                     }
-                </ol>
+                </div>
             </div>
             </div>
         </div>
