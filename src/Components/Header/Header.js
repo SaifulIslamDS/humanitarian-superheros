527fcf289fed1fc342d38c5b2fda92016e4d5bf5
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-09-27 12:49:27 +0600

    Two spelling updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <section className="header-container">
                 <div className="container">
                     <h1 className="display-3 site-title">International Fundraising Campaign!</h1>
-                    <h2 className="subtitle">Collecting funds to eliminate word's hunger </h2>
+                    <h2 className="subtitle">Collecting funds to eliminate world's hunger </h2>
                     <h2 className="subtitle">from The 9 Richest Person in the World!!</h2>
                     <h1 className="display-4 subtitle">Total Target Fund: 1000 Million!!!</h1>
                 </div>
